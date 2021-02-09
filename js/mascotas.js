c7f24a2525c061a7403f25b3eeb81c1380c56931
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-09 12:41:13 -0300

    dando funcionalidada al boton 'editar'

--- js/mascotas.js
+++ js/mascotas.js
@@ -5,6 +5,8 @@ const inputNombre = document.getElementById('input-nombre');
 const inputPropietario = document.getElementById('input-propie');
 const form = document.getElementById('form');
 const btnGuardar = document.getElementById('guardar');
+const indice = document.getElementById('indice');
+
 
 let mascotas = [
     {
@@ -15,21 +17,23 @@ let mascotas = [
 ];
 
 function listarMascotas () {
-    const htmlMascotas = mascotas.map((mascota, indice)=>`
+    const htmlMascotas = mascotas.map((mascota, index)=>`
         <tr>
-            <th scope="row">${indice}</th>
+            <th scope="row">${index}</th>
             <td>${mascota.tipo}</td>
             <td>${mascota.nombre}</td>
             <td>${mascota.propietario}</td>
             <td>
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-secondary"><i class="far fa-edit"></i></button>
+                <button type="button" class="btn btn-secondary editar"><i class="far fa-edit"></i></button>
                 <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
             </div>
             </td>
         </tr>
     `).join('');
     listaMascota.innerHTML = htmlMascotas;
+    
+    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
 }
 
 function enviarDatos(e) {
@@ -41,6 +45,12 @@ function enviarDatos(e) {
     };
     mascotas.push(datos);
     listarMascotas();
+}
+
+function editar(indice) {
+    return function handler() {
+        console.log(indice);
+    }
 }
 
 listarMascotas();
